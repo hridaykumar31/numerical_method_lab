2eb2fca787291582c897a8761866d052161271ba
hridaykumar31 <71826458+hridaykumar31@users.noreply.github.com>
2023-02-21 17:28:24 +0600

    Update range_kutta.cpp

--- range_kutta.cpp
+++ range_kutta.cpp
@@ -17,11 +17,11 @@ int main()
   h=(xn-x0)/n;
 
   for(int i=0;i<n;i++)
-  {
-      k1=((x0,y0))*h;
+  {43
+      k1=(f(x0,y0))*h;
       k2=(f((x0+h/2.0),(y0+k1/2.0)))*h;
       k3=(f((x0+h/2.0),(y0+k2/2.0)))*h;
-      k4=f(x0+h,y0+k3);
+      k4=f(x0+h,y0+k3)*h;
       k=(k1+2*k2+2*k3+k4)/6;
       yn=y0+k;
       cout<<x0<<" "<<y0<<" "<<yn<<endl;
